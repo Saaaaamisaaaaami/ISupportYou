3a31ca08c8055153467e4bff24080f0a89511de1
Saaaaamisaaaaami <samriddhilearns@gmail.com>
2024-11-12 14:44:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+<html>
+function toggleChat
+    const iframe = document.querySelector('.chatbot-iframe');
+    const button = document.querySelector('.chat-button');
+
+    if (iframe.style.display === 'none' || iframe.style.display === '') {
+        iframe.style.display = 'block';
+        button.textContent = 'Close AI Chat';
+    } else {
+        iframe.style.display = 'none';
+        button.textContent = 'Open AI Chat';
+    }
+</html>
